22359b92dda90d17120f760ba6d5190cf3b9b9db
Arun <arun22.chief@gmail.com>
2025-03-12 17:38:09 +0530

    going to work on auth

--- src/liveness.tsx
+++ src/liveness.tsx
@@ -20,7 +20,6 @@ export function LivenessQuickStartReact() {
           throw new Error(`Error fetching session ID: ${response.statusText}`);
         }
         const data = await response.json();
-      
         setCreateLivenessApiData(data);
       } catch (error) {
         console.error('Error fetching session ID:', error);
